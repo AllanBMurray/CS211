c9419443578de06625bd20d809ec8ecfa3a91a04
Allan Murray <allan.murray.2019@mumail.ie>
2019-02-24 23:07:03 +0000

    Changed file names.

---
+++ Lab00.java
@@ -0,0 +1,83 @@
+package CS211;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import java.math.*;
+
+/*
+ * Public Key (130 characters [0-9A-F]):
+04146AD91B050CB77A5164A3927165EFD987680C8F2A32E50883AB5609163D8D6EB1E0B509C4D362BFBDB0EA0D390885811C78405546A9BF635E17FDC917BC7F02
+Public Key (compressed, 66 characters [0-9A-F]):
+02146AD91B050CB77A5164A3927165EFD987680C8F2A32E50883AB5609163D8D6E
+ *
+ *Public Address: VwEYJvzkQmnMhnom18bNz4b6z5nHQ5t1VN
+ *Public Address Compressed: VmXdCKMnJFp9pi6yyENe8AZiXr6UG7aAH6
+ */
+public class Lab01 
+{
+	public static void main (String [] args)
+	{
+		String secret = "80"; //declare string and put "80" at the front.
+		String bitCharString = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"; //string for decimal to base58
+		String hexCharString = "0123456789ABCDEF"; //string for decimal to hex
+		String finalOutput = ""; //declare string to hold final answer. 
+		
+		for (int i = 0; i < 64; i++) //for loop to add 64 random numbers to secret in hex.
+		{
+			Random rand = new Random();
+			int num = rand.nextInt(16);
+			secret = secret + hexCharString.charAt(num);
+		}
+		
+		try
+		{
+			secret = secret + (sha256(sha256(secret))).substring(0,8); //put secret through sha256 hash twice and then add first 8 numbers of the result to the original string secret.
+		}catch (NoSuchAlgorithmException e){}
+		
+		System.out.println(secret);
+		System.out.println(secret.length());
+		
+		BigInteger bignumber = new BigInteger(1, hexStringToByteArray(secret)); //uses method to convert secret to byte array and then to BigInteger.
+		//BigInteger bignumber = new BigInteger("654321");
+		System.out.println(bignumber);
+
+		BigInteger BIFE = new BigInteger("58"); //declare useful BigInteger constants.
+		BigInteger BIZERO = new BigInteger("0");
+		System.out.println(bignumber.compareTo(BIZERO));
+		//while loop to convert bignumber to base58 final answer.
+		//58 is repeatedly divided into the number. The mod result at each step is added to finalOutput in base58.
+		while (bignumber.compareTo(BIZERO) > 0)
+		{
+			finalOutput = (bitCharString.charAt(bignumber.mod(BIFE).intValue())) + finalOutput;
+			bignumber = bignumber.divide(BIFE);
+		}
+		System.out.println(finalOutput);
+	}
+
+
+	static String sha256(String input) throws NoSuchAlgorithmException 
+	{
+		byte[] in = hexStringToByteArray(input);
+		MessageDigest mDigest = MessageDigest.getInstance("SHA-256");
+		byte[] result = mDigest.digest(in);
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < result.length; i++) 
+		{
+			sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+		}
+		return sb.toString();
+	}
+	
+	public static byte[] hexStringToByteArray(String s) 
+	{
+		int len = s.length();
+		byte[] data = new byte[len / 2];
+		for (int i = 0; i < len; i += 2) 
+		{
+			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+			+ Character.digit(s.charAt(i+1), 16));
+		}
+		return data;
+	}
+}
--- Lab01.java
+++ Lab01.java
@@ -1,83 +1,53 @@
 package CS211;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.*;
-import java.math.*;
 
-/*
- * Public Key (130 characters [0-9A-F]):
-04146AD91B050CB77A5164A3927165EFD987680C8F2A32E50883AB5609163D8D6EB1E0B509C4D362BFBDB0EA0D390885811C78405546A9BF635E17FDC917BC7F02
-Public Key (compressed, 66 characters [0-9A-F]):
-02146AD91B050CB77A5164A3927165EFD987680C8F2A32E50883AB5609163D8D6E
- *
- *Public Address: VwEYJvzkQmnMhnom18bNz4b6z5nHQ5t1VN
- *Public Address Compressed: VmXdCKMnJFp9pi6yyENe8AZiXr6UG7aAH6
- */
-public class Lab01 
+public class Lab02 
 {
 	public static void main (String [] args)
 	{
-		String secret = "80"; //declare string and put "80" at the front.
-		String bitCharString = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"; //string for decimal to base58
-		String hexCharString = "0123456789ABCDEF"; //string for decimal to hex
-		String finalOutput = ""; //declare string to hold final answer. 
+		System.out.println("Enter your sentence: ");
+		Scanner sc = new Scanner(System.in); //create scanner object
+		String testInput = sc.nextLine(); //set user input to variable testInput.
+		int[] freqList = new int[128]; //declare a int array with a length of 128.
+		int inputLen = testInput.length(); //set inputLen to length of string input by user.
+		int asciiCode; //declare variable to hold asciiCode value in for loop below.
+		sc.close();
 		
-		for (int i = 0; i < 64; i++) //for loop to add 64 random numbers to secret in hex.
+		for (int i = 0; i < inputLen; i++) //for loop to step through the characters in testInput one at a time.
 		{
-			Random rand = new Random();
-			int num = rand.nextInt(16);
-			secret = secret + hexCharString.charAt(num);
+			asciiCode = (int) testInput.charAt(i); //set asciiCode to the ASCII code of the char in testInput at position i.
+			freqList[asciiCode]++; //increment the freqList at the position associated with the value currently in asciiCode.
+			System.out.print(sevenDigits(Integer.toBinaryString(asciiCode))+" "); //print out the binary value (uses sevenDigits method).
+			if (i%7==6) //this causes the binary numbers to be printed with 7 values per line.
+			{
+				System.out.println();
+			}
 		}
 		
-		try
+		System.out.println("\n"+"\n"+"Letter Frequency");
+		for (int i = 0; i < 128; i++) //for loop to step through freqList array and print out the results.
 		{
-			secret = secret + (sha256(sha256(secret))).substring(0,8); //put secret through sha256 hash twice and then add first 8 numbers of the result to the original string secret.
-		}catch (NoSuchAlgorithmException e){}
-		
-		System.out.println(secret);
-		System.out.println(secret.length());
-		
-		BigInteger bignumber = new BigInteger(1, hexStringToByteArray(secret)); //uses method to convert secret to byte array and then to BigInteger.
-		//BigInteger bignumber = new BigInteger("654321");
-		System.out.println(bignumber);
-
-		BigInteger BIFE = new BigInteger("58"); //declare useful BigInteger constants.
-		BigInteger BIZERO = new BigInteger("0");
-		System.out.println(bignumber.compareTo(BIZERO));
-		//while loop to convert bignumber to base58 final answer.
-		//58 is repeatedly divided into the number. The mod result at each step is added to finalOutput in base58.
-		while (bignumber.compareTo(BIZERO) > 0)
-		{
-			finalOutput = (bitCharString.charAt(bignumber.mod(BIFE).intValue())) + finalOutput;
-			bignumber = bignumber.divide(BIFE);
-		}
-		System.out.println(finalOutput);
-	}
-
-
-	static String sha256(String input) throws NoSuchAlgorithmException 
-	{
-		byte[] in = hexStringToByteArray(input);
-		MessageDigest mDigest = MessageDigest.getInstance("SHA-256");
-		byte[] result = mDigest.digest(in);
-		StringBuffer sb = new StringBuffer();
-		for (int i = 0; i < result.length; i++) 
-		{
-			sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-		}
-		return sb.toString();
+			if (freqList[i]>1) //if the value at position i of freqList is great than 1 then the char is printed (using i as this is the char's ascii number).
+			{
+				System.out.println("'"+(char)i + "' appeared "+freqList[i]+" times");
+			}
+			else if(freqList[i]==1) //"times" is changed to "time" when the letter appears exactly once.
+			{
+				System.out.println("'"+(char)i + "' appeared "+freqList[i]+" time");
+			}
+		}	
 	}
 	
-	public static byte[] hexStringToByteArray(String s) 
-	{
-		int len = s.length();
-		byte[] data = new byte[len / 2];
-		for (int i = 0; i < len; i += 2) 
+	public static String sevenDigits(String a) //recursive algorithm used to add leading zeros to binary values with less then 7 digits.
+	{										   //binary values taken in and returned as Strings.
+		if (a.length() == 7) //exit condition
 		{
-			data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
-			+ Character.digit(s.charAt(i+1), 16));
+			return a;
 		}
-		return data;
+		else //recursion
+		{
+			return sevenDigits("0"+a);
+		}
 	}
 }
